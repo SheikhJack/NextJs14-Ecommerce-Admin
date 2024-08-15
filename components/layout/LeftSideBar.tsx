bb00da6e30d6e15fb4cda1c131a54ae3cb48e24d
SheikhJack <kealebogakido@gmail.com>
2024-08-15 22:50:06 +0200

    committed

--- components/layout/LeftSideBar.tsx
+++ components/layout/LeftSideBar.tsx
@@ -1,8 +1,8 @@
 "use client";
-import { UserButton } from '@clerk/nextjs'
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import { UserButton } from "@clerk/nextjs";
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
 import { usePathname } from 'next/navigation';
 
 import { navLinks } from '@/lib/constants';
@@ -12,7 +12,7 @@ function LeftSideBar() {
     const pathname = usePathname();
 
     return (
-        <div className='h-screen left-0 sticky p-10 flex flex-col gap-16 bg-blue-2 shadow-xl sm:hidden' >
+        <div className='h-screen left-0 sticky p-10 flex flex-col gap-16 bg-blue-2 shadow-xl max-lg:hidden' >
             <Image src="/logo.png" alt='logo' width={150} height={70} />
 
             <div className='flex flex-col gap-12'>
@@ -24,7 +24,6 @@ function LeftSideBar() {
             </div>
             <div className='flex gap-4 text-body-medium items-center'>
                 <UserButton />
-                <p>Edit Profile</p>
             </div>
 
         </div>
